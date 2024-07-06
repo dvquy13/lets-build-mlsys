85c50efda8c0ddaf6887f6256ae461054b82828a
Quy Dinh <quy.dinh3195@gmail.com>
2024-07-06 14:23:15 +0700

    WIP Set examples at Message in Modelfile

--- src/wrapper/v1.py
+++ src/wrapper/v1.py
@@ -85,3 +85,46 @@ Output:
         )
         output_json = {}
     return output_json
+
+
+def llm_extract_modelfile(input_texts: List[Dict]) -> Dict:
+    prompt = """
+Input:
+{input_texts}
+
+Output:
+"""
+    prompt = prompt.format(input_texts=str(input_texts))
+
+    response = log_time(printer=logger.debug, method_name='call_llm')(client.generate)(
+        model='ner_aspect_review',
+        prompt=prompt,
+        format='json'
+    )
+    output = response['response']
+    try:
+        output_json = json.loads(output)
+        (
+            logger
+            .opt(lazy=True)
+            .bind(input_texts=input_texts, llm_extracted=output)
+            .debug('[OUTPUT] LLM Extracted successfully')
+        )
+        input_texts_texts = [e['text'] for e in input_texts]
+        output_json_texts = [e['text'] for e in output_json.values()]
+        assert set(input_texts_texts) == set(output_json_texts), "Input texts do not match output texts"
+        assert_extracted_entity_is_subtext(output_json)
+    except json.decoder.JSONDecodeError:
+        error_msg = f"[COLLECT] {traceback.format_exc()}"
+        (
+            logger
+            .opt(lazy=True)
+            .bind(
+                input_texts=input_texts,
+                llm_extracted=output,
+                error_type='JSONOutputFormatError'
+            )
+            .error(error_msg)
+        )
+        output_json = {}
+    return output_json
