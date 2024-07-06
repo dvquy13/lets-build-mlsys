53d80992d6a8b9a3286900a63c72341132b69c39
Quy Dinh <quy.dinh3195@gmail.com>
2024-07-06 13:56:02 +0700

    ICE-12 WIP Modelfile

--- src/wrapper/v1.py
+++ src/wrapper/v1.py
@@ -52,7 +52,6 @@ Output:
         system=system_prompt,
         options=dict(
             temperature=0,
-            num_predict=1024,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
